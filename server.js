c848f1e33b9c6d97724297e098a4fa254ae4d8ce
Kevin thapa <bobmarley9461238@gmail.com>
2026-02-15 22:56:54 +0000

    Added feature to change status of user object

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const values = JSON.parse(fs.readFileSync("Data.json", "utf-8"))
 const args = process.argv
 const method = args[2] ? args[2].toLowerCase() : null
 
+
 let getting_id;
 let title;
 
-if (method === "add") {
+if (method === "add" || method === "list" ) {
     title = args[3]
 }
 
@@ -24,8 +25,8 @@ if (method === "update") {
     title = args[4]
 }
 
-if (method === "list") {
-    title = args[3]
+if (method === "mark-done"  || method === "mark-in-progress" || method === "mark-not-done" ) {
+    getting_id = Number(args[3])
 }
 
 
@@ -50,7 +51,7 @@ if (method === "list") {
     const users = {
         id: newId,
         name: title,
-        status: "todo",
+        status: "mark-not-done",
         timeCreated: day,
         updatedAt: day
     }
@@ -81,4 +82,73 @@ if (method === "update" && getting_id && title) {
     console.log("Task updated successfully!")
 
 }
+
+// Marking a task as done
+
+if (method === "mark-done" && getting_id) {
+
+    const finding_task = values.find(f => f.id === getting_id)
+
+    if (!finding_task) {
+        console.log("ID for task is not present!")
+        process.exit(0)
+    }
+
+    finding_task.status = "mark-done"
+    finding_task.updatedAt = new Date().toLocaleString()
+
+    fs.writeFileSync("Data.json", JSON.stringify(values, "", 2))
+
+    console.log("Status of task changed to mark-done")
+}
+
+if (method === "mark-in-progress" && getting_id) {
+
+    const finding_task = values.find(f => f.id === getting_id)
+
+    if (!finding_task) {
+        console.log("ID for task is not present!")
+        process.exit(0)
+    }
+
+    finding_task.status = "mark-in-progress"
+    finding_task.updatedAt = new Date().toLocaleString()
+
+    fs.writeFileSync("Data.json", JSON.stringify(values, "", 2))
+
+    console.log("Status of task changed to mark-in-progress")
+}
+
+if (method === "mark-not-done" && getting_id) {
+
+    const finding_task = values.find(f => f.id === getting_id)
+
+    if (!finding_task) {
+        console.log("ID for task is not present!")
+        process.exit(0)
+    }
+
+    finding_task.status = "mark-not-done"
+    finding_task.updatedAt = new Date().toLocaleString()
+
+    fs.writeFileSync("Data.json", JSON.stringify(values, "", 2))
+
+    console.log("Status of task changed to mark-not-done")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
